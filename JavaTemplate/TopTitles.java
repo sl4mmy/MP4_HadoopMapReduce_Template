1046f2e8206ea1f40979c824571fd58e10ec1e41
Kent R. Spillner <kspillne@illinois.edu>
2024-02-23 21:54:15 +0300

    mp4: fix ClassCastException
    Fix from https://campuswire.com/c/G8B7D15E6/feed/562

--- JavaTemplate/TopTitles.java
+++ JavaTemplate/TopTitles.java
@@ -188,7 +188,7 @@ public class TopTitles extends Configured implements Tool {
         @Override
         public void reduce(NullWritable key, Iterable<TextArrayWritable> values, Context context) throws IOException, InterruptedException {
             for (TextArrayWritable val : values) {
-                Text[] pair = (Text[]) val.toArray();
+                Writable[] pair = (Writable[]) []) val.toArray();
                 String word = pair[0].toString();
                 Integer count = Integer.parseInt(pair[1].toString());
 
