89178cb5438b01ede0e710d0a50a206744095481
Kent R. Spillner <kspillne@illinois.edu>
2024-02-24 20:30:45 +0300

    mp4: fix copy-pasta

--- JavaTemplate/TopPopularLinks.java
+++ JavaTemplate/TopPopularLinks.java
@@ -61,7 +61,7 @@ public class TopPopularLinks extends Configured implements Tool {
         jobB.setInputFormatClass(KeyValueTextInputFormat.class);
         jobB.setOutputFormatClass(TextOutputFormat.class);
 
-        jobB.setJarByClass(TopTitles.class);
+        jobB.setJarByClass(TopPopularLinks.class);
         return jobB.waitForCompletion(true) ? 0 : 1;
     }
 
